bdfaf65d0eb4b02c3d75e29d122cd73fe88a1674
Pauljls <paulazarsol@gmail.com>
2024-01-28 15:10:53 -0500

    version ESP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script';
 import './App.css';
 
 function App() {
+  const clientID = "292060742455-vr86dsqntspb8derafn4kkujsptmmeb1.apps.googleusercontent.com";
+
+  useEffect(() => {
+    const start = () => {
+      gapi.load('auth2', () => {
+        gapi.auth2.init({
+          clientId: clientID
+        });
+      });
+    };
+
+    gapi.load('client:auth2', start);
+  }, []); // Con esto habilitamos los servicios de Google
+
+  const onSuccess = (response) => {
+    console.log(response);
+  };
+
+  const onFailure = () => {
+    console.log("Algo salió mal");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      <div className='btn'>
+        <GoogleLogin
+          clientId={clientID}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={"single_host_policy"}
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+
